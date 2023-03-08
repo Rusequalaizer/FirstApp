28bc1a6d21473008efa887a8464e9ca29bbba78b
Rusequalaizer <olympkirill@gmail.com>
2023-03-09 01:34:01 +0400

    Uncomment function ConsoleInOut

--- src/Main.java
+++ src/Main.java
@@ -159,7 +159,7 @@ public class Main {
     public static void main(String[] args)
     {
         DataTypes();
-        // ConsoleInOut();
+        ConsoleInOut();
         ArithmeticalOperations();
     }
 }
